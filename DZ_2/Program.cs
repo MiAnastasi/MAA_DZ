1edfc424f4f717db45283d36027dcd4afed85fac
NastyaM <Nastya@mail.com>
2022-08-19 13:33:28 +0700

    DZ_3

--- DZ_2/Program.cs
+++ DZ_2/Program.cs
@@ -4,6 +4,7 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+
 Console.Write("Ввведите число №1: ");
 int firstNumber = Convert.ToInt32 (Console.ReadLine ());
 
---
+++ DZ_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Console.Write("Ввведите число №1: ");
+int firstNumber = Convert.ToInt32 (Console.ReadLine ());
+if (firstNumber%2==0)
+{
+    Console.WriteLine("да");
+}
+
+else 
+{
+    Console.WriteLine("нет");
+}
